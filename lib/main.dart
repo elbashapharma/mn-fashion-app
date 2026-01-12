171dba7ebc687a7d1a73dd88df6cc69413a9b039
pharma_vercel_app <odooelbasha@gmail.com>
2026-01-13 00:56:01 +0200

    fixmaindart

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,25 @@ import "screens/customers_screen.dart";
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const SheinPricingApp());
+  runApp(const MNFashionApp());
 }
 
-class SheinPricingApp extends StatelessWidget {
-  const SheinPricingApp({super.key});
+class MNFashionApp extends StatelessWidget {
+  const MNFashionApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "M&N Fashion",
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
         useMaterial3: true,
+        fontFamily: "Arial",
       ),
-      home: const CustomersScreen(),
+      // ✅ بدون const (عشان ما يطلع Error لو الشاشة مش const)
+      home: CustomersScreen(),
     );
   }
 }
+
